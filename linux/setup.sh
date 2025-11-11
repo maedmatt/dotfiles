b9a881e36c7d293f4080dd568779cbd8707a2963
Matteo Calabria <101880969+maedmatt@users.noreply.github.com>
2025-11-11 11:37:57 +0100

    update: setup.sh to include curl and uv installation
    Added curl installation and uv setup to the script.

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
 set -e
 
 apt update
-apt install neovim tmux -y
+apt install neovim tmux curl -y
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 cp .bashrc ~/
 cp .tmux.conf ~/
 mkdir -p ~/.config/nvim
