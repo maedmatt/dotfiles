bbe98aa705c0688358000ebec3dce788d9c01dd1
maedmatt <matteo.calabria01@gmail.com>
2025-12-26 11:53:56 +0100

    install tools script, add md/latex render

--- shared/nvim/lua/community.lua
+++ shared/nvim/lua/community.lua
@@ -8,5 +8,6 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   -- import/override with your plugins folder
 }
